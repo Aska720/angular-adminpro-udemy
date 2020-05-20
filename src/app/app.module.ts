be6dc3da28fd8a9373c794a96be6574098957968
Aitor Villar <aitor.villar@tlgcommerce.com>
2020-05-20 14:44:33 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotFoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    ChartsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
